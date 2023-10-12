3b27cb1dc25d540ab67dc6038281e31e884061d4
22piyush <piyushaglawe05@gmail.com>
2023-10-12 06:49:05 +0530

    footer imported

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Footer from './Components/Footer/Footer';
 import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-  hello
-  </>
+  <Footer/>
 );
 
