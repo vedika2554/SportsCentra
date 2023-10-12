01f408070c7ce51b07c2f239476a5363985df3ce
22piyush <piyushaglawe05@gmail.com>
2023-10-12 06:35:31 +0530

    Footer column completed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -34,6 +34,32 @@ function Footer(){
   	 				<li><a href="#">payment options</a></li>
   	 			</ul>
   	 		</div>
+               <div className="footer-col">
+  	 			<h4>Help</h4>
+  	 			<ul>
+  	 				<li><a href="#">FAQ</a></li>
+  	 				<li><a href="#">Terms & Condition</a></li>
+  	 				<li><a href="#">Privacy Policy</a></li>
+  	 				<li><a href="#">dress</a></li>
+  	 			</ul>
+  	 		</div>
+               <div className="footer-col">
+  	 			<h4>More Info</h4>
+  	 			<ul>
+  	 				<li><a href="#">Our Store</a></li>
+  	 				<li><a href="#">Special Offer</a></li>
+  	 				<li><a href="#">Recent Reviews</a></li>
+  	 				<li><a href="#">Trading Sports</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div className="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div className="social-links">
+  	 				<a href="#"><i className="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i className="fab fa-twitter"></i></a>
+  	 				<a href="#"><i className="fab fa-instagram"></i></a>
+  	 				<a href="#"><i className="fab fa-linkedin-in"></i></a>
+  	 			</div>
                 </div>
             </div>
         </footer>
