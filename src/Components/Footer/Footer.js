a334d5fd3548d2f08d3b7d71fac11d0b227218a1
22piyush <piyushaglawe05@gmail.com>
2023-10-12 06:28:56 +0530

    create all class for footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,6 +3,15 @@ import "./Footer.css";
 
 function Footer(){
     return(
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="footer-col">
+                        
+                    </div>
+                </div>
+            </div>
+        </footer>
 
     )
 }
