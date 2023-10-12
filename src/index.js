cd707c33f288cf3b67dae857ffb3020eab150154
22piyush <piyushaglawe05@gmail.com>
2023-10-12 06:58:13 +0530

    footer completed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Footer from './Components/Footer/Footer';
-import './index.css';
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Footer/>
+  <>
+  </>
 );
 
