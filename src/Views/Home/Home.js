081f53e22aed657fb5f59106a2c8964351539157
vedika2554 <vedikayadav652@gmail.com>
2023-10-13 10:07:21 +0530

    fifth commit

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -5,6 +5,13 @@ import imghockey from './../../Components/Image/hockey.jpg'
 import imgvollyball from './../../Components/Image/vollyball.jpg'
 import imgbaseball from './../../Components/Image/baseball.jpeg'
 import imgbasketball from './../../Components/Image/basketball.jpg'
+import imgbadminton from './../../Components/Image/badminton.jpg'
+import imgsketing from './../../Components/Image/sketing.jpg'
+import imgtennis from './../../Components/Image/tennis.jpg'
+import imgswim from './../../Components/Image/swimming.jpg'
+import imgcycle from './../../Components/Image/cycle.jpeg'
+import imgboxing from './../../Components/Image/boxing.jpg'
+
 
 
 import Navbar from './../../Components/Navbar/Navbar'
@@ -52,6 +59,50 @@ export default function Home()
  <div className='c1'>
    <img className='bas'src={imgbasketball }/>
    <h1 className='spga'>Basketball</h1>
+</div> 
+  </div>
+
+
+
+
+
+
+
+
+  <h1 className='t1'>Outdoor Sports</h1>
+            <hr className='h1'/>
+            <div className='container'>
+
+            <div className='c1'>
+               <img className='fot'src={imgbadminton }/>
+              <h1 className='spga'>Badminton</h1>
+            </div>
+
+            <div className='c1'>
+               <img className='bas'src={imgsketing}/>
+               <h1 className='spga'>Sketing</h1>
+            </div>
+
+             <div className='c1'>
+               <img className='fot'src={imgtennis}/>
+               <h1 className='spga'>Tennis</h1>
+            </div> 
+              </div>
+              <div className='container'>
+
+<div className='c1'>
+   <img className='fot'src={imgswim}/>
+   <h1 className='spga'>Swimming</h1>
+</div>
+
+<div className='c1'>
+   <img className='bas'src={imgcycle}/>
+   <h1 className='spga'>Cycling</h1>
+</div>
+
+ <div className='c1'>
+   <img className='cri'src={imgboxing}/>
+   <h1 className='spga'>Boxing</h1>
 </div> 
   </div>
   
