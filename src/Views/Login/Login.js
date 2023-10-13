71520581c82dd89a860cb059b481643c3ae58d3f
22piyush <piyushaglawe05@gmail.com>
2023-10-13 18:47:32 +0530

    start to create login and signup page in one page image imported

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Login.css";
 
-function Login(
+import user_icon from './../../Components/Image/person.png';
+import email_icom from './../../Components/Image/email.png';
+import user_icon from './../../Components/Image/password.png';
+
+
+function Login()
     {
+        return(
+            <div className="container">
+                <div className="header">
+                    <div className="text">{action}</div>
+                    <div className="underline"></div>
+                </div>
+                
+            </div>
+
+        )
 
     }
-)
+
 
 export default Login
