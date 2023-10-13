71dcbffb581469e1b85c15748a5e7ad6b49fb92b
vedika2554 <vedikayadav652@gmail.com>
2023-10-13 09:35:38 +0530

    4th commit

---
+++ src/Components/Productcard/Productcard.js
@@ -0,0 +1,19 @@
+import './Productcard.css'
+import imgsport from  './../Image/sporty-dla-poczatkujacych.jpg'
+export default function Productcard()
+{
+  return(
+    <div className='card'>
+         <h1><span className='a'>Sports Equipment</span><h1 className='b'>Up T0</h1><span className='c'> 30%-70% </span> <span className='g'> Off</span><p className='d'>UP TO 12 MONTHS | EXCHANGE OFFERS</p>
+<p className='e'>NO COST EMI | SCHEDULED DELIVERY</p>
+<p className='f'>COMBO OFFERS | BIG DISCOUNT</p>
+<div>
+<button className='btn1'>Buy now</button>
+<button className='btn2'>Trending Equipment</button>
+</div></h1>
+
+<img className='sport'src={ imgsport}/>
+
+    </div>
+  )
+}
--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,13 +1,65 @@
 import './Home.css'
+import imgcricket from './../../Components/Image/cricket.jpg'
+import imgFootball from './../../Components/Image/football.jpg'
+import imghockey from './../../Components/Image/hockey.jpg'
+import imgvollyball from './../../Components/Image/vollyball.jpg'
+import imgbaseball from './../../Components/Image/baseball.jpeg'
+import imgbasketball from './../../Components/Image/basketball.jpg'
+
+
 import Navbar from './../../Components/Navbar/Navbar'
 import Footer from './../../Components/Footer/Footer'
+import Productcard from './../../Components/Productcard/Productcard'
+
 export default function Home()
 {
     return(
-        <div>
-      <Navbar/>
-        <h1>home</h1>
-       <Footer/>
+        <>
+        <Navbar/>
+        <div className='home'>
+            <Productcard/>
+            <h1 className='t1'>Team Sports</h1>
+            <hr className='h1'/>
+            <div className='container'>
+
+            <div className='c1'>
+               <img className='cri'src={imgcricket}/>
+              <h1 className='spga'>Cricket</h1>
+            </div>
+
+            <div className='c1'>
+               <img className='fot'src={imgFootball}/>
+               <h1 className='spga'>Football</h1>
+            </div>
+
+             <div className='c1'>
+               <img className='fot'src={imghockey}/>
+               <h1 className='spga'>Hockey</h1>
+            </div> 
+              </div>
+              <div className='container'>
+
+<div className='c1'>
+   <img className='fot'src={imgvollyball}/>
+   <h1 className='spga'>Vollyball</h1>
+</div>
+
+<div className='c1'>
+   <img className='fot'src={imgbaseball}/>
+   <h1 className='spga'>Baseball</h1>
+</div>
+
+ <div className='c1'>
+   <img className='bas'src={imgbasketball }/>
+   <h1 className='spga'>Basketball</h1>
+</div> 
+  </div>
+  
+  
+
+            
+            <Footer/>
         </div>
+        </>
     )
 }
